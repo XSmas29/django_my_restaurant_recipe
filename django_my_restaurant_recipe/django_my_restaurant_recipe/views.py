1e48db75e78581220a7d0870c6bd711b1824fc14
Surya Bumantara <bumantara.surya2213@gmail.com>
2024-11-29 10:18:03 +0700

    add routing

--- django_my_restaurant_recipe/django_my_restaurant_recipe/views.py
+++ django_my_restaurant_recipe/django_my_restaurant_recipe/views.py
@@ -1,15 +1,15 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def home(request):
   # return HttpResponse('Hello, Django!')
-  return render(request, 'home.html')
+  return redirect('recipes')
 
 def recipes(request):
-  return HttpResponse('Recipes')
+  return render(request, 'recipes.html')
 
 def ingredients(request):
-  return HttpResponse('Ingredients')
+  return render(request, 'ingredients.html')
 
 def categories(request):
-  return HttpResponse('Categories')
+  return render(request, 'categories.html')
